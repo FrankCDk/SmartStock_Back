de453c27cd22679582f53991f42b00c0d46774d5
FrDk <frank28dk@gmail.com>
2024-11-10 17:21:01 -0500

    Verificacion de correcta creacion de proyecto

---
+++ SmartStock_Api/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartStock_Api.Controllers
+{
+
+    [ApiController]
+    [Route("/api/v1/product/[action]")]
+    public class ProductController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+    }
+}
